781d98a74cfed5fa473752286c045841dd0ef2a7
CosmoVibe <bufangliang@gmail.com>
2015-05-29 00:19:58 -0400

    test yo

---
+++ main.js
@@ -0,0 +1,27 @@
+var windowWidth = 800,
+	windowHeight = 600;
+var stage = new Kinetic.Stage({
+	container: 'container',
+	width: windowWidth,
+	height: windowHeight
+});
+
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+	x: 0,
+	y: 0,
+	width: 32,
+	height: 32,
+	stroke: 'black',
+	strokeWidth: 2,
+	fill: '#FFFF00'
+});
+
+layer.add(rect);
+stage.add(layer);
+
+socket.emit('talk', {'string': "HI SHELLEY LETS FUCK!"});
+socket.on('confirmation', function(data) {
+    console.log(data.msg);
+});
