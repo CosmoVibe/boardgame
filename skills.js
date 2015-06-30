5b6e672ca9a1d8a694d5ddfb13246c0624ff2e2d
sh3lztong <shelleytong1234@gmail.com>
2015-06-30 16:11:13 -0400

    skills.js added

---
+++ skills.js
@@ -0,0 +1,198 @@
+var boardgameserver = require('./boardgameserver.js');
+var serverUtility = require('./bgServerUtility.js'); 
+var units = require('./units.js').units;
+
+//skill variables
+//returns the index to the array of functions all skills
+
+//checks to see if the skill move 
+//unitPosition = position of your unit (coordinate)
+//targetPosition = position of the target (coordinate)
+function isWithinRange(unitPosition, targetPosition, range)
+{
+	var distance = [0, 0];	//initialize variables
+	distance[0] = unitPosition[0] - targetPosition[0];
+	distance[1] = unitPosition[1] - targetPosition[1];
+	console.log("unitPosition: " + unitPosition);
+	console.log("targetPosition: " + targetPosition);
+	console.log(distance); 
+
+	for (i = 0; i < range.length; i++)
+	{
+		if (distance.equals(range[i]))
+			return true; 
+	}
+
+	return false; 
+}
+
+//functions for checking ranges
+var checkRange = [
+	function(range, target, unitArray)	//enemy unit
+	{
+		var unitPosition = unitArray.position; 
+		targetPosition = units[serverUtility.getEnemyUnitIndex(boardgameserver.userID)][target].position;
+		return (isWithinRange(unitPosition, targetPosition, range));  
+	},
+	function(range, target, unitArray)	//ally unit
+	{
+		var unitPosition = unitArray.position;
+		targetPosition = units[serverUtility.getUnitIndex(boardgameserver.userID)][target].position;
+		return (isWithinRange(unitPosition, targetPosition, range));
+	},
+	function(range, target, unitArray)	//tile
+	{
+		var unitPosition = unitArray.position;	
+		return (isWithinRange(unitPosition, target, range));
+	}
+]; 
+
+//get index for checking ranges
+function getCheckRangeIndex(targetType)
+{
+	switch(targetType)
+	{
+		case 'enemy unit':
+			return 0; 
+		case 'ally unit':
+			return 1; 
+		case 'tile':
+			return 2; 
+	}
+}
+
+//check to see if the move is within range
+//skillArray = skill array
+function isWithinRangeCheck(skillArray, target, unitArray)
+{
+	var skillName = skillArray.name.toLowerCase();
+	var targetType = skillArray.target; 
+	console.log("Skill Name: " + skillName); 
+	console.log("Target Type: " + targetType);
+
+	var rangeIndex = getCheckRangeIndex(targetType);
+	console.log("Range Index: " + rangeIndex); 
+	console.log("Function: " + skillArray.range); 
+	return (checkRange[rangeIndex](skillArray.range, target, unitArray));
+}
+
+//skillName = name of the skill (ex: "Attack") *case sensitive*
+function getAllActionsIndex(actionName)
+{
+	switch (actionName)
+	{
+		case 'damage':
+			return 0; 
+		case 'movement':
+			return 1; 
+		case 'debuff':
+			return 2; 
+		case 'heal':
+			return 3;
+	}
+}
+
+var allActions = 
+[
+	function (actionArray, unitArray, target)	//damage
+	{
+		var thisPosition = unitArray.position;
+		var enemyUnit = units[serverUtility.getEnemyUnitIndex(boardgameserver.userID)][target]; 
+		var enemyPosition = enemyUnit.position; 
+		console.log("My position: " + thisPosition);
+		console.log("Enemy Position: " + enemyPosition);
+
+		console.log("Skill is within range");
+		console.log("Enemy HP Before: " + enemyUnit.hp); 
+		var ratio = actionArray.ratio;
+		console.log("Ratio: " + ratio);
+		console.log("Strength: " + unitArray.strength);   
+		var damage = unitArray.strength * ratio;
+		console.log("Damage: " + damage); 
+		enemyUnit.hp = enemyUnit.hp - damage; 
+		console.log("Enemy HP After: " + enemyUnit.hp); 
+	}
+]; 
+
+//is valid skill
+function isValidSkill(data)
+{
+	return ((data.skill != null) && (data.unit != null) && (data.target != null)); 	
+}
+
+//get skill
+//unit is an index
+//skillName is name of the skill
+function getSkillArray(skillName, unit)
+{
+	var index = serverUtility.getUnitIndex(boardgameserver.userID);
+	var skills = units[index][unit].skills; 
+	for (i = 0; i < skills.length; i++)
+	{
+		if (skills[i].name.toUpperCase() == skillName.toUpperCase())
+		{
+			return (skills[i]); 
+		}
+	}
+}
+
+//has enough skill energy
+//unitArray = array of the unit
+//skill = skill array of the unit (not an index)
+function hasSkillEnergy(unitArray, skillArray)
+{
+	console.log("Energy: " + unitArray.energy);
+	console.log("Skill Array: " + skillArray); 
+	console.log("Skill Cost: " + skillArray.cost); 
+
+	if (unitArray.energy >= skillArray.cost)
+	{
+		console.log("Has enough energy to do skill");
+		return true;
+	}
+	else
+	{
+		console.log("You don't have enough energy to move!");
+		return false; 
+	}
+}
+
+//process the skill move
+//returns true if processed, false if not
+//unit = index of the unit in array, units[]
+//skillArray = skill array
+function processSkillMove(unit, skillArray, target)
+{
+	var unitIndex = serverUtility.getUnitIndex(boardgameserver.userID);
+	var unitArray = units[unitIndex][unit];
+
+	if (hasSkillEnergy(unitArray, skillArray))
+	{
+		if (isWithinRangeCheck(skillArray, target, unitArray))
+		{
+			var actionArray = skillArray.action;
+			console.log("Action array: " + actionArray); 
+			for (i = 0; i < actionArray.length; i++)
+			{
+				var actionName = actionArray[i].type;
+				var actionIndex = getAllActionsIndex(actionName);
+				console.log("actionName: " + actionName);
+				console.log("actionIndex: " + actionIndex); 
+				allActions[actionIndex](actionArray[i], unitArray, target);
+			}
+
+			unitArray.energy = unitArray.energy - skillArray.cost;			
+			return true;
+		}
+	}
+	else
+		console.log("Not Enough Energy for skill");
+
+	return false; 
+}
+
+module.exports.isWithinRange = isWithinRange;
+module.exports.isValidSkill = isValidSkill; 
+module.exports.getSkillArray = getSkillArray; 
+module.exports.hasSkillEnergy = hasSkillEnergy; 
+module.exports.processSkillMove = processSkillMove;
